2233114303df1f45b927637540ae0ea67eb86d36
Alisson <cardoso_barros16@hotmail.com>
2019-10-20 18:48:25 -0300

    chore: Altera retorno da rota

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = required('express');
 const app = express();
 
 app.get('/teste', (req, res) => {
-  return res.json({hello: 'world'});
+  return res.json({hello: 'Alisson'});
 });
 
 app.listem(3333);
